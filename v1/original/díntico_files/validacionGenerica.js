2456ec3f2e15de334cf5305ad90477d4ff41b7ca
diegograssino <diegograssino@gmail.com>
2021-10-19 12:44:05 -0300

    first commit

---
+++ v1/original/díntico_files/validacionGenerica.js
@@ -0,0 +1,21 @@
+﻿/////CREACION Y ELEIMINACION DE MENSAJES
+function ponerMensajeDeError(ErrorAMostrar, NombreDeObjeto) {
+    $("#" + NombreDeObjeto).addClass("input-validation-error");
+    $("#" + NombreDeObjeto).removeClass("valid");
+    $("#" + NombreDeObjeto).attr("aria-required", "true");
+    $("#" + NombreDeObjeto).attr("aria-describedby", NombreDeObjeto + "-error");
+    $("#" + NombreDeObjeto).attr("aria-invalid", "true");
+    $("#" + NombreDeObjeto).parent().children("span").removeClass("field-validation-valid");
+    $("#" + NombreDeObjeto).parent().children("span").addClass("field-validation-error");    
+    $("#" + NombreDeObjeto).parent().children("span").html("<span id='" + NombreDeObjeto + "-error'>" + ErrorAMostrar + "</span>").css("color", "#b94a48");
+}
+
+function sacarMensajeDeError(NombreDeObjeto) {
+    $("#" + NombreDeObjeto).removeClass("input-validation-error");
+    $("#" + NombreDeObjeto).removeAttr("aria-required");
+    $("#" + NombreDeObjeto).removeAttr("aria-describedby");
+    $("#" + NombreDeObjeto).removeAttr("aria-invalid");
+    $("#" + NombreDeObjeto).parent().children("span").removeClass("field-validation-error");
+    $("#" + NombreDeObjeto).parent().children("span").addClass("field-validation-valid");
+    $("#" + NombreDeObjeto + "-error").remove();
+}
---
+++ v1/original/díntico_files/validarFormularioDeRegistro.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+    $("#idFormulario").submit(function (e) {
+        e.preventDefault();        
+        var result = true;
+
+        if ($('#g-recaptcha-response').val() == "") {
+            $("#capchaptObligatorio").show();
+            result = false;
+        }
+        else {
+            $("#capchaptObligatorio").hide();
+        }
+       
+        if ($("#UserName").valid() == false) {
+            result = false;
+        }
+        if ($("#Email").valid() == false) {
+            result = false;
+        }
+        if ($("#Password").valid() == false) {
+            result = false;
+        }
+        if ($("#ConfirmPassword").valid() == false) {
+            result = false;
+        }
+        if (result == true) {
+            var form = this;
+            form.submit();
+        }
+    });
+
+    $("#UserName").blur(function () {
+        $("#UserName").valid();
+    });
+    $("#Email").blur(function () {
+        $("#Email").valid();
+    });
+    $("#Password").blur(function () {
+        $("#Password").valid();
+    });
+    $("#ConfirmPassword").blur(function () {
+        $("#ConfirmPassword").valid();
+    });  
+});
